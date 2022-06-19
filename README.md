f8593c6b9a477a0b95d18e5c69bd9b99f449acd1
Galileu Kim <galileukim@gmail.com>
2022-06-19 14:24:07 -0400

    finalize data cleaning process

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # social_finance
+## intro:
 
 This is a repository for the interview exercise on recidivism rates.
 
@@ -6,3 +7,7 @@ The research questions are:
 
 1. What percentage of individuals released in 2010 were incarcerated again within 1 year, within 2 years, and within 3 years?
 2. How do the recidivism rates you originally calculated vary by risk level?
+
+## replication:
+
+This repository has been initiated with [renv](https://rstudio.github.io/renv/articles/renv.html). Please run: `renv::restore()` to install all package requirements.
---
+++ source/data_preprocess.R
@@ -0,0 +1,123 @@
+# ==============================================================================
+# data preprocessing
+# ==============================================================================
+# set-up
+library(dplyr)
+library(readr)
+library(lubridate)
+library(ggplot2)
+library(here)
+
+set.seed(1789)
+
+# ------------------------------------------------------------------------------
+# 1.0. load-in data
+# ------------------------------------------------------------------------------
+release_files <- dir_ls(
+    here("data", "input"),
+    regexp = ".*releases.*.csv$"
+)
+
+releases_raw <- read_csv(
+    release_files
+)
+
+risk_score_raw <- read_csv(
+    here("data", "input", "risk_score.csv")
+)
+
+# ------------------------------------------------------------------------------
+# 2.0. data processing: releases
+# ------------------------------------------------------------------------------
+# fix dates
+releases <- releases_raw %>%
+    # transform into date
+    mutate(
+        across(
+            ends_with("date"),
+            ~ parse_date_time(., orders = "d-b-Y")
+        )
+    ) %>%
+    arrange(
+        pers_id, sentence_date
+    ) %>%
+    # generate case_id if sentence_date and release_date are identical
+    group_by(pers_id, sentence_date, release_date) %>%
+    mutate(
+        case_id = cur_group_id()
+    ) %>%
+    ungroup() %>%
+    # sort by pers_id and case_id
+    arrange(
+        pers_id, case_id
+    ) %>%
+    select(
+        case_id, pers_id, everything()
+    )
+
+# eliminate cases where release date is less than sentence date
+# 32 obs.
+releases <- releases %>% 
+    filter(
+        release_date > sentence_date
+    ) %>%
+    # create lags for release and sentence dates
+    group_by(pers_id) %>%
+    mutate(
+        lag_sentence_date = lag(sentence_date, order_by = case_id),
+        lag_release_date = lag(release_date, order_by = case_id) 
+    ) %>%
+    ungroup()
+
+# generate date cutpoints for earliest release in 2010
+first_release_date <- releases %>%
+    group_by(pers_id) %>%
+    summarise(
+        original_release_date = min(release_date),
+        original_release_date_plus_1 = original_release_date + years(1),
+        original_release_date_plus_2 = original_release_date + years(2),
+        original_release_date_plus_2 = original_release_date + years(3)
+    ) %>%
+    ungroup()
+
+# identify 2010 cohort
+cohort_2010 <- first_release_date %>%
+    filter(
+        year(original_release_date) == 2010
+    )
+
+# releases of cohort of 2010
+releases_cohort_2010 <- releases %>% 
+    inner_join(
+        cohort_2010,
+        by = "pers_id"
+    )
+
+# ------------------------------------------------------------------------------
+# 2.1. data-processing: risk-score
+# ------------------------------------------------------------------------------
+# clean risk score data
+# note: there are people with multiple risk scores
+# maintain them if these are identical, otherwise drop (19 obs.)
+risk_score <- risk_score %>% 
+    group_by(pers_id) %>%
+    mutate(
+        distinct_risk = n_distinct(risk)
+    ) %>%
+    ungroup() %>%
+    filter(distinct_risk == 1) %>%
+    distinct(pers_id, .keep_all = TRUE) %>% 
+    select(-distinct_risk)
+
+# ------------------------------------------------------------------------------
+# 3.0. write-out
+# ------------------------------------------------------------------------------
+releases_cohort_2010 %>% 
+    write_csv(
+        here("data", "output", "release_cohort_2010.csv")
+    )
+
+risk_score %>%
+    write_csv(
+        here("data", "output", "risk_score.csv")
+    )
